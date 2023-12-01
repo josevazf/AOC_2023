8b9514d002d861d2a202f20c0fa5cabc1e4da9b2
jrocha-v <jrocha-v@student.42porto.com>
2023-12-01 23:28:01 +0000

    day1_part2

--- day1/part2/main.c
+++ day1/part2/main.c
@@ -1,32 +1,90 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+int	ft_strncmp(const char *s1, const char *s2, size_t size)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < size && (s1[i] == s2[i]) && s1[i] != '\0')
+		i++;
+	if (i < size)
+		return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	else
+		return (0);
+}
+
+int	ncompare(char *str)
+{
+	if (!ft_strncmp(str, "one", 3))
+		return (1);
+	if (!ft_strncmp(str, "two", 3))
+		return (2);
+	if (!ft_strncmp(str, "three", 5))
+		return (3);
+	if (!ft_strncmp(str, "four", 4))
+		return (4);
+	if (!ft_strncmp(str, "five", 4))
+		return (5);
+	if (!ft_strncmp(str, "six", 3))
+		return (6);
+	if (!ft_strncmp(str, "seven", 5))
+		return (7);
+	if (!ft_strncmp(str, "eight", 5))
+		return (8);
+	if (!ft_strncmp(str, "nine", 4))
+		return (9);
+	return (-1);
+}
 
 int	main(void)
 {	
 	FILE	*fp = fopen("input.txt", "r");
-	char	*line = NULL;
-	char	*str = NULL;
 	size_t	len = 0;
-	char	*numbers = "0123456789";
+	bool	vstart;
+	bool	vend;
+	char	*line = NULL;
+	int		i;
 	int		count = 0;
-	int		value1 = 0;
-	int		value2 = 0;
+	int		v1 = 0;
+	int		v2 = 0;
 
 	while (getline(&line, &len, fp) != -1)
 	{
-		str = strpbrk(line, numbers);
-		value1 = str[0] - 48;
-		value2 = value1;
-		while (str)
+		i = -1;
+		vstart = false;
+		vend = false;
+		while (line[++i])
 		{
-			value2 = atoi(str);
-			str = strpbrk(++str, numbers);			
+			if (isdigit(line[i]) && !vstart)
+			{
+				v1 = line[i] - 48;
+				vstart = true;
+			}
+			else if (ncompare(line + i) != -1 && !vstart)
+			{
+				v1 = ncompare(line + i);
+				vstart = true;
+			}
+			else if (isdigit(line[i]) && vstart)
+			{
+				v2 = line[i] - 48;
+				vend = true;
+			}
+			else if (ncompare(line + i) != -1 && vstart)
+			{
+				v2 = ncompare(line + i);
+				vend = true;
+			}
+			if (!vend)
+				v2 = v1;
 		}
-		count += ((value1 * 10) + value2);
+		count += ((v1 * 10) + v2);
 	}
 	fclose(fp);
-	free(str);
 	free(line);
 	printf("Sum: %d\n", count);
 }
