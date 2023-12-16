eea504a145d065492a1c46247338b3ff4b11e30b
jrocha-v <jrocha-v@student.42porto.com>
2023-12-16 09:54:14 +0000

    day7_part1 ok

---
+++ day7/part1/includes/day7_1.h
@@ -0,0 +1,11 @@
+#ifndef DAY7_1_H
+# define DAY7_1_H
+
+#include "../../../libft/libft.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+#endif
